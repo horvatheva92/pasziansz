7f51f7b83b464fe2f5a19baf7f26874b453245e3
Évi <vi>
2017-02-05 13:52:40 +0100

    alapok

---
+++ sugo.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+   <title> Pasziánsz </title>
+   <meta charset= "utf-8">
+   <link rel="stylesheet" href="sugo.css"/>
+   <script src="jquery-3.1.1.min.js"></script>
+</head>
+<body bgcolor="#87ADD3" text="black">
+ <div class="informacio">
+ <div style="width:1355px; height:620px; overflow:scroll;">    
+ <h1>Játék szabályok</h1>
+ <p class="justify">Pasziánsznak nevezzük az egyszemélyes kártyajátékokat. Rengeteg fajtájuk van (és a fajtákon belül is sok különböző variáció), de a játékmenet mindegyiknél az,hogy 
+az összekevert kártyalapokat valamilyen rend szerint az asztalra helyezzük, majd a szabályok betartásával megpróbáljuk egy másik rendbe – ez a gyakorlatban általában
+csökkenő vagy növekvő sorrendet jelent – áthelyezni őket.A szabályok és az alapelrendezés határozzák meg a játék nehézségét. Vannak pasziánszjátékok ahol,
+ha elég ügyesek vagyunk minden leosztás kirakható és vannak olyanok,ahol bármennyire is jól játszunk, az adott leosztást lehetetlen megfelelően megcsinálni.</p><br>
+
+<p class="justify">A játékokat francia kártyával játszák: 1, 2 esetleg 3 vagy 4 pakli felhasználásával. 
+Egy pakli 52 lapból áll, ezek 4 szín között oszlanak meg (♠ pikk, ♣ treff, ♥ kör, ♦ káró). <br>
+Minden színhez 13 lap tartozik és általában ebben az értéksorrendben követik egymást: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K <br>
+Egyes játékváltozatokban az ász a sor végén van.</p>
+
+<h2>Pasziánsz fogalmak</h2>
+<p class="justify">A pasziánszjátékokban a kezdőálláshoz az összekevert csomagból leosztjuk a lapokat az adott játékra jellemző alakzatban. 
+Ezeket a leosztott lapokat nevezzük tablónak (más néven asztal vagy tábla).
+ A megmaradt lapokat – ha olyan a játék, hogy maradnak lapok – talonnak nevezzük.
+ Innen tesszük a lapokat az adott szabályok szerint tablóra, illetve egyes játékokban (például a Klondikében) először 
+ a segédtalonra, majd onnan a tablóra. 
+ Alapnak nevezzük (vagy halomnak) azt a területet, illetve azokat a lapokat, ahova az átrendezés során a többi lapot gyűjtjük
+ össze. A pasziánszok célja, hogy az összes lapot elhelyezzük az alapokban.</p>
+<div><img src=pasziansz_jatek_fogalmak.png></div>
+</body>
+</html>
