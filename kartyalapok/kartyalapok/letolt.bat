5af81e69e6bdd5bbe78f489a6c82ce1a6eeebe7c
Évi <vi>
2017-04-11 15:34:55 +0200

    újítás

--- kartyalapok/kartyalapok/letolt.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set loopcount=13
-:loop
-"C:\Program Files (x86)\GnuWin32\bin\"wget http://0v.hu/apps/pasziansz/img/cards/s%loopcount%.png -P d:\k
-set /a loopcount=loopcount-1
-if %loopcount%==0 goto exitloop
-goto loop
-:exitloop
